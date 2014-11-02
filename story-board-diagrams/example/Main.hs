84cb57ed8de0af929c5fa3fc0640617f13e1ec24
Gabor Greif <ggreif@gmail.com>
2014-11-02 19:14:22 +0100

    play around with shapes

--- story-board-diagrams/example/Main.hs
+++ story-board-diagrams/example/Main.hs
@@ -9,7 +9,8 @@ import qualified Graphics.Storyboard as SB
 import           Graphics.Storyboard.Diagrams()
 
 main :: IO ()
-main = SB.storyBoard 3000 [ slide ]
+--main = SB.storyBoard 3000 [ slide, slide2 ]
+main = SB.storyBoard 3000 [ slide2 ]
 
 slide :: SB.Slide ()
 slide = do
@@ -39,3 +40,21 @@ arrowOpts = with & gaps  .~ small
 tournament :: Int -> Diagram B R2
 tournament n = decorateTrail (regPoly n 1) (map node [1..n])
   # applyAll [connectOutside' arrowOpts j k | j <- [1 .. n-1], k <- [j+1 .. n]]
+
+--default (Double)
+
+slide2 :: SB.Slide ()
+slide2 = do
+      SB.align SB.center $ SB.p $ "Diagrams Plugin Example2"
+
+      let t = SB.drawTile (200,200) (pic # applyAll [connectOutside' arrowOpts (0 :: Int) (1 :: Int)])
+          pic :: Diagram B R2
+          pic = (r <> vrule 1 <> (c1 === strutY 1 === c0)) ||| hrule 1
+          r = square 5
+          c = circle 0.2 # fc red
+          c0 = c # named (0 :: Int)
+          c1 = c # named (1 :: Int)
+
+      SB.place SB.top (SB.nudge SB.top SB.center t)
+
+      return ()
