02cac1cf0392ec9362adafd0ed02df692d0f1920
Gabor Greif <ggreif@gmail.com>
2014-12-13 12:27:58 +0100

    WIP: first ideas

--- story-board-diagrams/src/Graphics/Storyboard/Diagrams.hs
+++ story-board-diagrams/src/Graphics/Storyboard/Diagrams.hs
@@ -7,6 +7,7 @@ module Graphics.Storyboard.Diagrams where
 import Graphics.Storyboard.Types
 
 import Diagrams.Backend.Canvas
+import Diagrams.Backend.SVG
 import Diagrams.Prelude
 
 instance Drawing (Diagram Canvas R2) where
@@ -17,3 +18,19 @@ instance Drawing (Diagram Canvas R2) where
           w', h' :: Int
           w' = round w
           h' = round h
+
+instance Drawing (Diagram SVG R2) where
+   drawCanvas (w,h) d = renderSVGDia Canvas opts d
+    where opts :: Options Canvas R2
+          opts = CanvasOptions $ Dims (fromIntegral w')
+                                      (fromIntegral h')
+          w', h' :: Int
+          w' = round w
+          h' = round h
+
+-- OLD:
+-- http://stackoverflow.com/questions/3768565/drawing-a-svg-file-on-a-html5-canvas
+-- NEW:
+-- http://getcontext.net/read/svg-images-on-a-html5-canvas
+
+renderSVGDia Canvas opts d = undefined
